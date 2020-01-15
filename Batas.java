a8de44f85c2ee378d0a3991b95209e0d8d9162fc
Aerunisa <fitrianichaerunisaun@gmail.com>
2020-01-15 09:12:53 +0700

    test

--- Batas.java
+++ Batas.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 public class Batas {
     public static void main(String[] args) {
-        // TODO code application logic here
-        int batas,i=0;        
-        Scanner input= new Scanner(System.in);
+        int[] tabInt = {51,23,17,32,43,88};
+        int i,j,data_sisip;
         
-        System.out.println(" Masukkan Batas Blangan");
-        batas =input.nextInt();
+        for(i=1; i<6; i++){ 
+            data_sisip = tabInt[i]; 
+            j = i; 
+            while((j>0) && (tabInt[j-1] > data_sisip)){            
+                tabInt[j] = tabInt[j-1]; 
+                j = j - 1; 
+            }        
+            tabInt[j] = data_sisip; 
+        } 
+        for(i=0; i<6; i++){ 
+            System.out.print(tabInt[i]+" ");
         
-        for (i=0;batas>=1;batas=batas-2){
-            if((batas%2==0)){
-                System.out.println(batas);
-        }else if((batas%1==0)){
-                System.out.println(batas-1);
-        }
     }
     }   
 }
